37d5689853887709ac3505e5f0b829dda68f56c8
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-02-21 18:03:45 +0800

    Update env.py

--- env.py
+++ env.py
@@ -3,4 +3,4 @@ import os
 os.environ['USERNAME'] = '175598874' # Your new Maya ID here
 os.environ['PASSWORD'] = 'password12345' # Your Maya Password
 os.environ['PATH'] = 'E:\Selenium\ChromeDriver' # Path to your ChromeDriver
-os.environ.get("HEADLESS") == 'True' # Change to 'False' if you like to visualise it
+os.environ["HEADLESS"] == 'True' # Change to 'False' if you like to visualise it
